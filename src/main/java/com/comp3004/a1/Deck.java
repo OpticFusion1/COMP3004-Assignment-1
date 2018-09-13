3c411009c13322d9ee1e188b75437c9a5687434a
GabeJensen <gabriel.jensen11@hotmail.ca>
2018-09-12 22:05:29 -0400

    main: implemented Deck class - Full deck of cards is now generated - Shuffle function implemented - Cards can now be removed individually from the top of the deck

--- src/main/java/com/comp3004/a1/Deck.java
+++ src/main/java/com/comp3004/a1/Deck.java
@@ -1,5 +1,55 @@
 package com.comp3004.a1;
 
-public class Deck {
+import java.util.Random;
 
+public class Deck {
+	private String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
+	private String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+	private Card[] cards;
+	private final int DECK_SIZE = 52;
+	private int index = 0;
+	
+	public Deck() {
+		cards = new Card[DECK_SIZE];
+		generateDeck();
+	}
+
+	private void generateDeck() {
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 13; j++) {
+				cards[j + i * 13] = new Card(ranks[j], suits[i]);
+			}
+		}
+	}
+	
+    /*
+    Name: ShuffleDeck
+    Purpose: Shuffle the deck to a random order
+     */
+    public void shuffleDeck(){
+        Random random = new Random();
+        int change;
+        random.nextInt();
+
+        //Starting at index 0, each card is swapped with a random card at a larger index
+        for(int i = 0; i < DECK_SIZE; i++){
+            change = i + random.nextInt(DECK_SIZE - i);
+            swap(i,change);
+        }
+    }
+
+    private void swap(int i, int change){
+        Card temp = cards[i];
+        cards[i] = cards[change];
+        cards[change] = temp;
+    }
+    
+    public Card dealCard() {
+		if(index == DECK_SIZE) {
+			return null;
+		}
+		
+		return cards[index++];
+	}
+	
 }
